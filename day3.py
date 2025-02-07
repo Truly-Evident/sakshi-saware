23ec770fe102029141dc44b169e45cfe002d7eb3
Truly-Evident <146549011+Truly-Evident@users.noreply.github.com>
2025-02-07 12:44:56 +0530

    day3.py

--- day3.py
+++ day3.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title('Welcome to Python website')
 st.write('This is a simple website to show how to use Python with Streamlit')
 
-data=pd.DataFrame({'c1':[1,2,3,4,5],'c2':["A","B",30,40,50]})
+data=pd.DataFrame({'c1':[1,2,3,4,5],'c2':["A","B","C","D","E"]})
 st.write(data)
 chart_data = np.random.randn(20, 3)
 st.bar_chart(chart_data)
